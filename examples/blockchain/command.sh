4ae8e7eecbde7afbf04bc35e19b7d5c9dbc601b9
jolyonjian <1453402455@qq.com>
2024-11-21 17:52:13 +0800

    add the example for TEE

---
+++ examples/blockchain/command.sh
@@ -0,0 +1,1 @@
+python fetch_papers.py --keywords blockchain --categories 1,2,3,4,5 --level A
---
+++ examples/tee/command.sh
@@ -0,0 +1,1 @@
+python fetch_papers.py --keywords TEE,SGX,TrustZone,Confident --categories 1,2,3,4,5 --level A
